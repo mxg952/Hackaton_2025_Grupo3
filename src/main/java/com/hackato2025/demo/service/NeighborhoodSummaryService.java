403ed90058ada9026417012d09310065884b4056
luriguso <luriguso@gmail.com>
2025-10-15 18:24:36 +0200

    feat: add SummaryNeighborhood service and controller

--- src/main/java/com/hackato2025/demo/service/NeighborhoodSummaryService.java
+++ src/main/java/com/hackato2025/demo/service/NeighborhoodSummaryService.java
@@ -11,20 +11,20 @@ import java.util.List;
 import java.util.Map;
 
 @Service
-public class NoiseLevelService {
-    private List<Map<String, Object>> noiseLevel = new ArrayList<>();
+public class NeighborhoodSummaryService {
+    private List<Map<String, Object>> summaryData = new ArrayList<>();
 
     @PostConstruct
     public void loadData() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        InputStream inputStream = getClass().getResourceAsStream("/data/noiseLevel.json");
+        InputStream inputStream = getClass().getResourceAsStream("/data/neighborhoodSummary.json");
         if (inputStream == null) {
-            throw new RuntimeException("File noiseLevel.json not found in resources/data/");
+            throw new RuntimeException("File neighborhoodSummary.json not found in resources/data/");
         }
-        noiseLevel = mapper.readValue(inputStream, new TypeReference<>() {});
+        summaryData = mapper.readValue(inputStream, new TypeReference<>() {});
     }
 
-    public List<Map<String, Object>> getHutOfAllNeighborhoods() {
-        return noiseLevel;
+    public List<Map<String, Object>> getSummaryData() {
+        return summaryData;
     }
 }
